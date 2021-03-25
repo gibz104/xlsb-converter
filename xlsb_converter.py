79985160fee28ebe92f145452ac62b6162543359
Ross G <35471104+rgbson104@users.noreply.github.com>
2021-03-24 19:28:38 -0500

    refactor

--- xlsb_converter.py
+++ xlsb_converter.py
@@ -3,6 +3,16 @@
 import win32com.client as win32
 import os
 
+def getFiles(directory):
+    'given a directory, will return list of all files in given directory'
+    fileList = []
+    if directory[-1] != '\\':
+        directory = directory + '\\'
+    for item in os.scandir(directory):
+        if item.is_file():
+            fileList.append(directory + item.name)
+    return fileList
+
 def convertFiles(fileList):
     'given a list of file paths, will save .xls, .xlsx, .xlsm, or .csv files as .xlsb in same directory'
     for file in fileList:
@@ -21,16 +31,6 @@ def convertFiles(fileList):
 
             xlApp.Quit()
             print(f'Saved {tgtPath} from {file}')
-
-def getFiles(directory):
-    'given a directory, will return list of all files in given directory'
-    fileList = []
-    if directory[-1] != '\\':
-        directory = directory + '\\'
-    for item in os.scandir(directory):
-        if item.is_file():
-            fileList.append(directory + item.name)
-    return fileList
 
 def main(directory):
     'given a windows formatted folder path, will convert all .xls, .xlsx, .xlsm, and .csv files in that directory
